424dae129eb4b93e7c60d83cccbb17e4bc621b4d
Joel S Joy <52008820+joelsjoyt@users.noreply.github.com>
2021-07-06 16:24:12 +0530

    Update user.py
    Implemented Class Inheritance

--- user/user.py
+++ user/user.py
@@ -1,10 +1,21 @@
-class User:
+class Department:
+    def __init__(self, name = None, authority = None ):
+        self.department_name = name
+        self.authority = authority
+    
+    def show_department(self):
+        print(f'Department name: {self.name} || Authority: {self.authority}')
+
+
+class User(Department):
     def __init__(self, name = None, id = None):
+        super().__init__("ADS", "Classified")
         self.name = name
         self.id = id
     
     def show_user(self):
         print(f'User name: {self.name}, user_id: {self.id}')
+        print(f'Deparment name: {self.department_name}, Authority: {self.authority}')
 
 def main():
     user1 = User("Test",10)
